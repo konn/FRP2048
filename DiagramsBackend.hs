8732e0dbf1cdda18d155ca713e1639e293e37cc3
Hiromi Ishii <konn.jinro@gmail.com>
2014-04-13 02:44:50 +0900

    SVGFont

--- DiagramsBackend.hs
+++ DiagramsBackend.hs
@@ -1,7 +1,7 @@
 {-# LANGUAGE CPP, FlexibleContexts, ForeignFunctionInterface  #-}
 {-# LANGUAGE MultiParamTypeClasses, NoMonomorphismRestriction #-}
 {-# LANGUAGE OverloadedStrings                                #-}
-module DiagramsBackend (renderBoard) where
+module DiagramsBackend (drawBoard, renderBoard) where
 import           Control.Lens             ((&), (.~))
 import           Data.Color               (Color (..), _Hue)
 import           Data.Color.Names
@@ -12,16 +12,14 @@ import           Data.Default             (def)
 import           Data.Monoid              (mempty)
 import qualified Data.Text.Lazy           as T
 import           Diagrams.Backend.SVG
-import           Diagrams.Prelude         (Any, Path, QDiagram, R2, Renderable)
-import           Diagrams.Prelude         (SizeSpec2D (..), atop, fc, hcat', lc)
-import           Diagrams.Prelude         (lw, renderDia, sep, square, text)
-import           Diagrams.Prelude         (vcat', ( # ))
-import qualified Diagrams.TwoD.Text       as D
+import           Diagrams.Prelude         (Any, Diagram, Path, QDiagram, R2)
+import           Diagrams.Prelude         (Renderable, SizeSpec2D (..), atop)
+import           Diagrams.Prelude         (fc, hcat', lc, lw, renderDia, sep)
+import           Diagrams.Prelude         (square, stroke, vcat', ( # ))
+import           Graphics.SVGFonts
+import           JavaScript.JQuery        (JQuery, setHeight, setHtml, setWidth)
 import           Puzzle
 import           Text.Blaze.Renderer.Text (renderMarkup)
-
-import JavaScript.JQuery (setHtml)
-import JavaScript.JQuery (JQuery)
 
 sqSize :: Double
 sqSize = 100
@@ -33,24 +31,28 @@ canvasSize :: Double
 canvasSize = sqMargin*3 + sqSize*4
 
 renderBoard :: Board -> JQuery -> IO JQuery
-renderBoard bd el =
-   let opts = SVGOptions Absolute Nothing
-       svg = T.toStrict $ renderMarkup $  renderDia SVG opts $ drawBoard $ bd
-   in setHtml svg el
+renderBoard bd el = do
+  let opts = SVGOptions Absolute Nothing
+      svg = T.toStrict $ renderMarkup $  renderDia SVG opts $ drawBoard $ bd
+  setHtml svg el
+  setWidth  canvasSize el
+  setHeight canvasSize el
 
-drawBoard :: (Renderable D.Text b, Renderable (Path R2) b)
-          => Board -> QDiagram b R2 Any
+drawBoard :: Renderable (Path R2) b => Board -> QDiagram b R2 Any
 drawBoard =
   vcat' (def & sep .~ sqMargin) .
   map (hcat' (def & sep .~ sqMargin) . map drawCell) .
   toLists
 
-drawCell :: (Renderable D.Text b, Renderable (Path R2) b)
-         => Maybe Int -> QDiagram b R2 Any
+drawCell :: Renderable (Path R2) b => Maybe Int -> QDiagram b R2 Any
 drawCell mint =
-  let num  = maybe mempty (text . show) mint
+  let num  = maybe mempty (myText . show) mint
       cell = square sqSize # fc (calcHue mint) # lc C.black # lw 1.0
   in num `atop` cell
+
+myText :: Renderable (Path R2) b  => String -> Diagram b R2
+myText s = stroke (textSVG' $ TextOpts s bit INSIDE_W KERN False (sqSize*0.5) (sqSize*0.5))
+           # fc C.black
 
 calcHue :: Maybe Int -> C.Colour Double
 calcHue Nothing = C.white
